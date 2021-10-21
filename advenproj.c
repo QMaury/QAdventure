708940c2042d5c87af2f9742440fe78e029d9dcc
QMaury <87496773+QMaury@users.noreply.github.com>
2021-10-20 19:13:12 -0700

    Add files via upload

--- advenproj.c
+++ advenproj.c
@@ -2,19 +2,41 @@
 #include <stdlib.h>
 
 int answer;
+int sword = 0;
 
 int main() {
-	printf("hello, sinner\n\n");
+	printf("Adventure 0.2 - QMaury\n\n");
 	
-	printf("1) Go left\n2) Go right\n? ");
-	scanf("%d", &answer );
+	printf("1) Go west\n2) Go east\n? ");
+	scanf("%d", &answer);
 
 	if(answer == 1) {
-		printf("> Damnation!\n");
+		printf("> You are dead.\n");
 	}
 	else if(answer == 2) {
-		printf("> Salvation!\n");
+		printf("\n1) Go north\n2) Go east\n? ");
+		scanf("%d", &answer);		/*  I am already beginning to see that */
+		if(answer == 1) {		/*  this code is fucking retarded.  */
+			printf("> You've been killed by a ghoul.\n> Game over.\n");
+			return 0;
+		}
+		else if(answer == 2) {
+			printf("> You find a sword\n\n");
+			int sword = 1;
+			printf("1) Go west\n? ");
+			scanf("%d", &answer);
+			if(answer == 1) {   /*	GOD I AM A FUCKING RETARDED  */
+				printf("\n1) Go north\n2) Go east\n? ");
+				scanf("%d", &answer);
+				if(answer == 1) {
+					printf("> You encounter a ghoul.\n> You kill the ghoul.\n");
+					printf("> You win!\n");
+					return 0;
+				}
 	}
-	
+		}
+	}
+	else {
+		printf("> invalid answer\n");
+	}
 }
-
